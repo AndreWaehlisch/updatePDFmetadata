f4a12d5725a7dae22ff54310877cec0966cb9d9c
André Wählisch <andre.waehlisch@physik.tu-berlin.de>
2018-09-29 16:08:29 +0200

    allow custom key updates with the --custom flag

--- updatepdfmetadata.sh
+++ updatepdfmetadata.sh
@@ -4,16 +4,28 @@ meNAME="updatePDFmetadata"
 me="-$meNAME: "
 defaultdumpFILE="./meta.dump"
 defaultoutFILE="./out.pdf"
+theIFS=';'
 
 sedSCRIPT () {
+	if [[ $1 = *"$theIFS"* ]]; then
+		IFS=$theIFS
+		array=($1)
+		unset IFS
+		oldKEY="${array[0]}"
+		newKEY="${array[1]}"
+	else
+		oldKEY="$1"
+		newKEY="${@:2}"
+		newKEY="$2"
+	fi
+	newKEY=$(echo "$newKEY" | recode ..xml)
 	# sed cmd from: https://stackoverflow.com/a/18620241/5769953
-	newKEY=$(echo "$2" | recode ..xml)
 	newKEY=$(sed -e 's/[\/&]/\\&/g' <<< $newKEY)
-	echo ' -e '\''/InfoKey: '$1'/{n;s/\(InfoValue: \).*/\1'"$newKEY"'/}'\'
+	echo ' -e '\''/InfoKey: '$oldKEY'/{n;s/\(InfoValue: \).*/\1'"$newKEY"'/}'\'
 }
 
 # getopt code from: https://stackoverflow.com/a/16483297/5769953
-TEMP=`getopt -o hpda:t:s: --long print-pdfinfo,dont-clean,help,author:,title:,subject: -n "$meNAME" -- "$@"`
+TEMP=`getopt -o hpda:t:s:c: --long print-pdfinfo,dont-clean,help,author:,title:,subject:,custom: -n "$meNAME" -- "$@"`
 if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
 eval set -- "$TEMP"
 
@@ -22,9 +34,17 @@ while true ; do
 		-a|--author) sedscriptSTRING+=$(sedSCRIPT Author "$2") ; shift 2 ;;
 		-t|--title) sedscriptSTRING+=$(sedSCRIPT Title "$2") ; shift 2 ;;
 		-s|--subject) sedscriptSTRING+=$(sedSCRIPT Subject "$2"); shift 2 ;;
+		-c|--custom)
+			if [[ $2 = *"$theIFS"* ]]; then
+				sedscriptSTRING+=$(sedSCRIPT "$2");
+				shift 2;
+			else
+				echo "When using the --custom flag you need to also use the separator (i.e. \"$theIFS\").";
+				exit 1;
+			fi;;
 		-p|--print-pdfinfo) printpdfinfoFLAG=1 ; shift ;;
 		-d|--dont-clean) dontcleanFLAG=1 ; shift ;;
-		-h|--help) echo "Usage: $0 [--author <author>] [--title <title>] [--subject <subject>] [--dont-clean] [--print-pdfinfo] <PDFfile> [<dumpFILE>] [<outFILE>]" ; exit 0 ;;
+		-h|--help) echo "Usage: $0 [--author <author>] [--title <title>] [--subject <subject>] [--custom \"<PDF key>,<value>\"] [--dont-clean] [--print-pdfinfo] <PDFfile> [<dumpFILE>] [<outFILE>]" ; exit 0 ;;
 		--) shift ; break ;;
 		*) echo $me"Error handling options." ; exit 1 ;;
 	esac
